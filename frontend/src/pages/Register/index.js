2b8bd53be18e263c28639fac2fefe41ec262c66a
heltonjohn07 <heltonjohn07@hotmail.com>
2020-03-26 23:05:50 -0300

    Mobile da aplicação

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -37,9 +37,9 @@ export default function Register(){
 
                     <h1>Cadastro</h1>
                     <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG</p>
-                    <Link className="back-link" to='/register'>
+                    <Link className="back-link" to='/'>
                         <FiArrowLeft size={16} color="#e02041" />
-                        Não tenho Cadastro
+                        Voltar para Home
                     </Link>
                 </section>
                 <form onSubmit={handleRegister} >
